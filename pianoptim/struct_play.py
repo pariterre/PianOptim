df595ff02cc20e7a196eca1c65acae8ea0fb4f84
Pariterre <pariterre@hotmail.com>
2024-08-07 16:00:22 -0400

    Added bed phase

--- pianoptim/struct_play.py
+++ pianoptim/struct_play.py
@@ -15,34 +15,12 @@ from bioptim import (
     OdeSolver,
     Solver,
     Axis,
-    ShowOnlineType,
+    OnlineOptim,
+    PlotType,
 )
-import numpy as np
 
 from utils.pianist import Pianist
 from utils.dynamics import PianistDyanmics
-
-
-# Joint indices in the biomechanical model:
-# 0. Pelvic Tilt: Anterior (+) Tilt / Posterior (-) Tilt
-# 1. Thoracic Flexion/Extension: Flexion (+) / Extension (-)
-# 2. Thoracic Rotation: Left (+) / Right (-)
-# 3. Upper Thoracic (Rib Cage) Flexion/Extension: Flexion (+) / Extension (-)
-# 4. Upper Thoracic (Rib Cage) Rotation: Left (+) / Right (-)
-# 5|0 Shoulder Flexion/Extension: Flexion (-) / Extension (+)
-# 6|1 Shoulder Abduction/Adduction: Abduction (+) / Adduction (-)
-# 7|2 Shoulder Internal/External Rotation: Internal (+) / External (-)
-# 8|3 Elbow Flexion/Extension: Flexion (-) / Extension (+)
-# 9|4 Forearm Pronation/Supination: Pronation (Left +) / Supination (Right -)
-# 10|5 Wrist Flexion/Extension: Flexion (-) / Extension (+)
-# 11|6 Metacarpophalangeal (MCP) Flexion/Extension: Flexion (-) / Extension (+)
-
-# Description of movement phases:
-# Phase 0: Preparation - Getting the fingers in position.
-# Phase 1: Key Descend - The downward motion of the fingers pressing the keys.
-# Phase 2: Key Bed - The phase where the keys are fully pressed and meet the bottom.
-# Phase 3: Key Release (Upward) - Releasing the keys and moving the hand upward.
-# Phase 4: Return to Neutral (Downward) - Bringing the fingers back to a neutral position, ready for the next action.
 
 
 class ZeroPosition(Enum):
@@ -52,37 +30,21 @@ class ZeroPosition(Enum):
 
 def prepare_ocp(
     model_path: str,
-    n_phases: int,
     n_shootings: tuple[int, ...],
-    phase_times: tuple[float, ...],
+    min_phase_times: tuple[float, ...],
+    max_phase_times: tuple[float, ...],
     block_trunk: bool,
     zero_position: ZeroPosition,
     ode_solver: OdeSolver,
 ) -> OptimalControlProgram:
 
-    trunk_dof = range(6)
+    n_phases = 5
 
     prep_phase = 0
     down_phase = 1
-    up_phase = 2
-    end_phase = 3
-
-    tau_min, tau_max = -40, 40
-
-    # Control the inputs
-    if len(n_shootings) < n_phases:
-        raise ValueError(
-            f"The number of shooting points ({len(n_shootings)}) must be greater than or equal to "
-            f"the number of phases ({n_phases})"
-        )
-    n_shootings = n_shootings[:n_phases]
-
-    if len(phase_times) < n_phases:
-        raise ValueError(
-            f"The number of phase times ({len(phase_times)}) must be greater than or equal to "
-            f"the number of phases ({n_phases})"
-        )
-    phase_times = phase_times[:n_phases]
+    hold_phase = 2
+    up_phase = 3
+    end_phase = 4
 
     pianist_models: list[Pianist] = []
     dynamics = DynamicsList()
@@ -97,20 +59,29 @@ def prepare_ocp(
         model = Pianist(model_path)
         pianist_models.append(model)
 
-        if phase == down_phase:
-            dynamics.add(PianistDyanmics.configure_forward_dynamics_with_external_forces, phase=phase)
-        elif phase == up_phase:
+        if phase in (down_phase, hold_phase, up_phase):
             dynamics.add(PianistDyanmics.configure_forward_dynamics_with_external_forces, phase=phase)
         else:
             dynamics.add(DynamicsFcn.TORQUE_DRIVEN, phase=phase)
 
         x_bounds.add("q", bounds=model.bounds_from_ranges("q"), phase=phase)
-        x_init.add("q", model.q_for_hand_over_keyboard, phase=phase)
+        x_init.add("q", model.q_hand_on_keyboard, phase=phase)
+
         x_bounds.add("qdot", bounds=model.bounds_from_ranges("qdot"), phase=phase)
-        u_bounds.add("tau", min_bound=[tau_min] * model.nb_tau, max_bound=[tau_max] * model.nb_tau, phase=phase)
+        u_bounds.add("tau", bounds=model.joint_torque_bounds, phase=phase)
 
         # Minimization to convexify the problem
-        objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", phase=phase, weight=0.01)
+        objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", phase=phase, weight=0.001)
+
+        # Some of the phases should be done as fast as possible
+        if min_phase_times[phase] != max_phase_times[phase]:
+            objective_functions.add(
+                ObjectiveFcn.Mayer.MINIMIZE_TIME,
+                phase=phase,
+                min_bound=min_phase_times[phase],
+                max_bound=max_phase_times[phase],
+                weight=100,
+            )
 
     # The first and last frames are at rest
     x_bounds[0]["qdot"][:, 0] = 0
@@ -119,7 +90,7 @@ def prepare_ocp(
     # The trunk should not move if requested
     if block_trunk:
         for phase in range(n_phases):
-            x_bounds[phase]["qdot"][trunk_dof, :] = 0
+            x_bounds[phase]["qdot"][pianist_models[phase].trunk_dof, :] = 0
 
     # Start with the finger on the key
     if zero_position == ZeroPosition.MARKER:
@@ -129,10 +100,9 @@ def prepare_ocp(
             node=Node.START,
             first_marker="finger_marker",
             second_marker="Key1_Top",
-            target=np.array([0, 0, -0.1]),
         )
     elif zero_position == ZeroPosition.Q:
-        x_bounds[prep_phase]["q"][:, 0] = model.q_for_hand_over_keyboard
+        x_bounds[prep_phase]["q"][:, 0] = pianist_models[prep_phase].q_hand_on_keyboard
     else:
         raise ValueError("Invalid initial position")
 
@@ -152,57 +122,47 @@ def prepare_ocp(
         marker_index="finger_marker",
     )
 
-    # The key should be at its bottom at the end of the press phase
-    if n_phases > down_phase:
-        objective_functions.add(
-            ObjectiveFcn.Mayer.TRACK_MARKERS,
-            phase=down_phase,
-            node=Node.END,
-            marker_index="finger_marker",
-            quadratic=False,
-            axes=Axis.Z,
-            weight=1000,
-        )
+    # The key should be fully pressed during the hold phase
+    objective_functions.add(
+        ObjectiveFcn.Lagrange.SUPERIMPOSE_MARKERS,
+        phase=hold_phase,
+        first_marker="finger_marker",
+        second_marker="key1_base",
+        axes=Axis.Z,
+        quadratic=False,
+        weight=-1000,
+    )
 
-    # The key should be fully lifted at the end of the up phase (press_phase
-    if n_phases > up_phase:
-        # Lift the finger from the key bed up to the top of the key
+    # The key should be fully lifted at the end of the up phase (press_phase)
+    constraints.add(
+        ConstraintFcn.SUPERIMPOSE_MARKERS,
+        phase=up_phase,
+        node=Node.END,
+        axes=Axis.Z,
+        first_marker="finger_marker",
+        second_marker="Key1_Top",
+    )
+
+    # The body should be at its final position at the end of the last phase
+    if zero_position == ZeroPosition.MARKER:
         constraints.add(
             ConstraintFcn.SUPERIMPOSE_MARKERS,
-            phase=up_phase,
+            phase=end_phase,
             node=Node.END,
-            axes=Axis.Z,
             first_marker="finger_marker",
-            second_marker="Key1_Top",
+            second_marker="key1_above",
         )
-
-    if n_phases > end_phase:
-        if zero_position == ZeroPosition.MARKER:
-            constraints.add(
-                ConstraintFcn.SUPERIMPOSE_MARKERS,
-                phase=end_phase,
-                node=Node.END,
-                first_marker="finger_marker",
-                second_marker="Key1_Top",
-                target=np.array([0, 0, -0.1]),
-            )
-        elif zero_position == ZeroPosition.Q:
-            x_bounds[end_phase]["q"][:, -1] = model.q_for_hand_over_keyboard
-        else:
-            raise ValueError("Invalid final position")
-
-    # Add time minimization
-    for phase in [prep_phase, down_phase, up_phase, end_phase]:
-        objective_functions.add(
-            ObjectiveFcn.Mayer.MINIMIZE_TIME, phase=phase, min_bound=0, max_bound=phase_times[phase], weight=1000
-        )
+    elif zero_position == ZeroPosition.Q:
+        x_bounds[end_phase]["q"][:, -1] = model.q_hand_above_keyboard
+    else:
+        raise ValueError("Invalid final position")
 
     # Prepare the optimal control program
-    return OptimalControlProgram(
+    ocp = OptimalControlProgram(
         bio_model=pianist_models,
         dynamics=dynamics,
         n_shooting=n_shootings,
-        phase_time=phase_times,
+        phase_time=[(n + m) / 2 for n, m in zip(min_phase_times, max_phase_times)],
         x_bounds=x_bounds,
         u_bounds=u_bounds,
         x_init=x_init,
@@ -213,31 +173,41 @@ def prepare_ocp(
         n_threads=6,
     )
 
+    # Add a graph that shows the finger height
+    for phase in range(n_phases):
+        ocp.add_plot(
+            "Finger height",
+            lambda t0, phases_dt, node_idx, x, u, p, a, d: pianist_models[phase].compute_marker_from_dm(
+                x[: pianist_models[phase].nb_q], "finger_marker"
+            )[2, :],
+            phase=phase,
+            plot_type=PlotType.INTEGRATED,
+        )
+
+    return ocp
+
 
 def main():
     model_path = "./models/pianist.bioMod"
     block_trunk = False
-    n_phases = 4
-    n_shooting = (100, 50, 50, 100)
-    phase_time = (0.1, 0.05, 0.05, 0.1)
+    n_shooting = (20, 20, 50, 20, 20)
+    min_phase_time = (0.05, 0.01, 0.05, 0.01, 0.05)
+    max_phase_time = (0.10, 0.05, 0.05, 0.05, 0.10)
     zero_position = ZeroPosition.MARKER
-    ode_solver = OdeSolver.RK4(n_integration_steps=1)
+    ode_solver = OdeSolver.RK4(n_integration_steps=5)
 
     ocp = prepare_ocp(
         model_path=model_path,
-        n_phases=n_phases,
         block_trunk=block_trunk,
         n_shootings=n_shooting,
-        phase_times=phase_time,
+        min_phase_times=min_phase_time,
+        max_phase_times=max_phase_time,
         zero_position=zero_position,
         ode_solver=ode_solver,
     )
     ocp.add_plot_penalty(CostType.ALL)
 
-    solv = Solver.IPOPT(
-        show_online_optim=True,
-        show_options={"type": ShowOnlineType.SERVER},
-    )
+    solv = Solver.IPOPT(online_optim=OnlineOptim.MULTIPROCESS_SERVER, show_options={"show_bounds": True})
     solv.set_maximum_iterations(500)  # TODO This should not be necessary
     # solv.set_linear_solver("ma57")
 
